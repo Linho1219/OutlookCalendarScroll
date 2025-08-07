c119e11e183ceb8f3322fae449b00ad2f510c8e9
Linho <lhp@linho.cc>
2025-08-07 16:29:22 +0800

    metadata 更新

--- userscript.meta.ts
+++ userscript.meta.ts
@@ -1,13 +1,28 @@
-export const metadata = `
-// ==UserScript==
-// @name         Outlook Scroll Navigation
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Scroll to switch calendar months in Outlook PWA
-// @author       You
-// @match        https://outlook.live.com/*
-// @match        https://outlook.office.com/*
-// @grant        none
-// @run-at       document-end
-// ==/UserScript==
-`.trim();
+const metadataObject: Metadata = {
+  name: "Outlook Scroll Navigation",
+  namespace: "http://tampermonkey.net/",
+  version: "0.1",
+  description: "Scroll to switch calendar months in Outlook PWA",
+  author: "You",
+  match: ["https://outlook.live.com/*", "https://outlook.office.com/*"],
+  grant: "none",
+  "run-at": "document-end",
+};
+
+interface Metadata {
+  [key: string]: string | string[];
+}
+
+function generateMetadataStr(metadata: Metadata): string {
+  const keyLength = Math.max(...Object.keys(metadata).map((k) => k.length));
+  const lines = Object.entries(metadata).flatMap(([key, value]) => {
+    const paddedKey = key.padEnd(keyLength);
+    if (!Array.isArray(value)) value = [value];
+    return value.map((value) => `// @${paddedKey}  ${value}`);
+  });
+  lines.unshift("// ==UserScript==");
+  lines.push("// ==/UserScript==");
+  return lines.join("\n");
+}
+
+export const metadata = generateMetadataStr(metadataObject);
